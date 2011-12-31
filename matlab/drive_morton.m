bd0264bf24b1af4ed7b0356109f553c71141926f
Evan Bollig <bollig@gmail.com>
2011-12-30 22:03:33 -0700

    Ok, this code DOES hash nodes to a morton ordering, but we have to keep in mind that the MORTON index is across a VIRTUAL array which means it extends beyond the dimensions of the physical array. That said, we need to adjust our morton indices in order to properly sort the nodes

--- matlab/drive_morton.m
+++ matlab/drive_morton.m
@@ -1,15 +1,17 @@
 DIM = 3;
-n=2;
-NX = 2^n; 
+NX = 2; 
 if DIM == 1
-    [nodeX, nodeY, nodeZ] = meshgrid(linspace(0,1,NX),0,0);
+    %[nodeX, nodeY, nodeZ] = meshgrid(linspace(0,1,NX),0,0);
+    [nodeX, nodeY, nodeZ] = meshgrid(1:NX,0,0);
     [ijkX, ijkY, ijkZ] = meshgrid(1:NX,0,0); 
 elseif DIM == 2
-    [nodeX, nodeY, nodeZ] = meshgrid(linspace(0,1,NX),linspace(0,1,NX),0);
+    %[nodeX, nodeY, nodeZ] = meshgrid(linspace(0,1,NX),linspace(0,1,NX),0);
+    [nodeX, nodeY, nodeZ] = meshgrid(1:NX,1:NX,0);
     [ijkX, ijkY, ijkZ] = meshgrid(1:NX,1:NX,0); 
 elseif DIM==3
-    [nodeX, nodeY, nodeZ] = meshgrid(linspace(0,1,NX),linspace(0,1,NX),linspace(0,1,NX));
-    [ijkX, ijkY, ijkZ] = meshgrid(1:NX,1:NX,0);
+    [nodeX, nodeY, nodeZ] = meshgrid(1:NX,1:NX,1:NX);
+   % [nodeX, nodeY, nodeZ] = meshgrid(linspace(0,1,NX),linspace(0,1,NX),linspace(0,1,NX));
+    [ijkX, ijkY, ijkZ] = meshgrid(1:NX,1:NX,1:NX);
 else
     fprintf('Error, No support for dimensions greater than 3\n');
     return;
@@ -18,11 +20,13 @@ end
 nodes = [nodeX(:), nodeY(:), nodeZ(:)];
 ijk_ind = [ijkX(:), ijkY(:), ijkZ(:)];
 
+%subplot(2,1,1)
 plot3(nodes(:,1), nodes(:,2), nodes(:,3),'o-');
 
 morton_ind = ijk_to_morton(ijk_ind, DIM, [0,1]);
 
-s_nodes = nodes(morton_ind+1,:); 
+s_nodes = nodes(morton_ind,:); 
+%subplot(2,1,2)
 plot3(s_nodes(:,1), s_nodes(:,2), s_nodes(:,3),'o-');
 
 %plot(nodes(morton_ind,1), nodes(morton_ind,2),'o-');
--- matlab/ijk_to_morton.m
+++ matlab/ijk_to_morton.m
@@ -1,17 +1,25 @@
 function [morton_ind] = ijk_to_morton(ijk_ind, dims, boundingBox)
+I = ijk_ind(:,1)-1; 
+J = ijk_ind(:,2)-1; 
+K = ijk_ind(:,3)-1;
+
 if (dims < 2)
     morton_ind = ijk_ind(:,1); 
 else
+    %ORIG=[dec2bin(ijk_ind(:,1)), dec2bin(ijk_ind(:,2))]
     
-    X = dilate(ijk_ind(:,1)-1, dims-1, 1);
-    Y = dilate(ijk_ind(:,2)-1, dims-1, 1);
-    
+    X = dilate(I, dims-1, 1);
+    Y = dilate(J, dims-1, 1);
+    %dec2bin(I)
+    %dec2bin(X)
+    %dec2bin(J)
+    %dec2bin(Y)
     if (dims > 2)
-        Z = dilate(ijk_ind(:,3), dims-1, 1);
-        morton_ind = bitor(bitor(bitshift(X,2), bitshift(Y,1)), Z);  
+       Z = dilate(K, dims-1, 1);
+       morton_ind = bitor(bitor(bitshift(X,2), bitshift(Y,1)), Z) + 1;  
     else 
-       morton_ind = bitor(bitshift(X,1), Y);  
+       morton_ind = bitor(bitshift(X,1),Y) + 1; 
     end
-   
+   %dec2bin(sort(morton_ind))
 end
 end
