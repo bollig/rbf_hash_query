c0a3d00f04a6ffe2afae7a96d22607e007011758
Evan Bollig <bollig@gmail.com>
2011-12-31 02:37:31 -0700

    U ordering works but its a little backwards. need to flip indices

--- matlab/drive_morton.m
+++ matlab/drive_morton.m
@@ -1,5 +1,5 @@
 DIM = 3;
-NX = 128; 
+NX = 4; 
 if DIM == 1
     %[nodeX, nodeY, nodeZ] = meshgrid(linspace(0,1,NX),0,0);
     [nodeX, nodeY, nodeZ] = meshgrid(1:NX,0,0);
@@ -23,11 +23,13 @@ ijk_ind = [ijkX(:), ijkY(:), ijkZ(:)];
 %subplot(2,1,1)
 plot3(nodes(:,1), nodes(:,2), nodes(:,3),'o-');
 
-morton_ind = ijk_to_morton(ijk_ind, DIM, [0,1]);
+morton_ind = ijk_to_u(ijk_ind, DIM, [0,1]);
 
 [temp morton_compressed_ind] = sort(morton_ind); 
 s_nodes = nodes(morton_compressed_ind,:); 
 %subplot(2,1,2)
 plot3(s_nodes(:,1), s_nodes(:,2), s_nodes(:,3),'o-');
-
+xlabel('k');
+ylabel('i');
+zlabel('j');
 %plot(nodes(morton_ind,1), nodes(morton_ind,2),'o-');
--- matlab/ijk_to_morton.m
+++ matlab/ijk_to_morton.m
@@ -1,25 +1,23 @@
-function [morton_ind] = ijk_to_morton(ijk_ind, dims, boundingBox)
+function [output_ind] = ijk_to_morton(ijk_ind, dims, boundingBox)
+%% Produces the O_Z-order (Morton Ordering)
 I = ijk_ind(:,1)-1; 
 J = ijk_ind(:,2)-1; 
 K = ijk_ind(:,3)-1;
 
+%% We dont want more than 1 neighbor in the same dimension before switching
+%% to other dimensions
+edge = 1;
+
 if (dims < 2)
-    morton_ind = ijk_ind(:,1); 
+    output_ind = ijk_ind(:,1); 
 else
-    %ORIG=[dec2bin(ijk_ind(:,1)), dec2bin(ijk_ind(:,2))]
-    
-    X = dilate(I, dims-1, 1);
-    Y = dilate(J, dims-1, 1);
-    %dec2bin(I)
-    %dec2bin(X)
-    %dec2bin(J)
-    %dec2bin(Y)
+    X = dilate(I, edge^(dims-1), edge);
+    Y = dilate(J, edge^(dims-1), edge);
     if (dims > 2)
-       Z = dilate(K, dims-1, 1);
-       morton_ind = bitor(bitor(bitshift(X,2), bitshift(Y,1)), Z) + 1;  
+       Z = dilate(K, edge^(dims-1), edge);
+       output_ind = bitor(bitor(bitshift(X,edge^(dims-1)), bitshift(Y,edge^(dims-2))), Z) + 1;  
     else 
-       morton_ind = bitor(bitshift(X,1),Y) + 1; 
+       output_ind = bitor(bitshift(X,edge^(dims-1)),Y) + 1; 
     end
-   %dec2bin(sort(morton_ind))
 end
 end
