14267e8232d9eab216cc8fea105d8658dd913cf4
Evan Bollig <bollig@gmail.com>
2012-12-13 23:55:08 -0600

    Not sure all the changes but backing them in case.

---
+++ matlab/int_morton/ijk_to_ijk.m
@@ -0,0 +1,19 @@
+function [output_ind] = ijk_to_z(ijk_ind, dims)
+%% Produces a O_ijk-order (Raster)
+I = ijk_ind(:,1)-1; 
+J = ijk_ind(:,2)-1; 
+K = ijk_ind(:,3)-1;
+
+%% We dont want more than 1 neighbor in the same dimension before switching
+%% to other dimensions
+edge = dims-1;
+
+if (dims < 2)
+    output_ind = ijk_ind(:,1); 
+else
+    if (dims > 2)
+    else 
+
+    end
+end
+end
