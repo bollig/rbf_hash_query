3e23518256619df65cc5355fabdb508e3c51a575
Evan BOllig <bollig@gmail.com>
2013-06-18 02:11:45 -0400

    Generated all orderings in 1d, 2d and 3d with dilation. Also generating equivalent SYMRCM matrix to compare bandwidth

--- matlab/drive_lsh.m
+++ matlab/drive_lsh.m
@@ -4,7 +4,7 @@ DIM = 3;
 % Number of nodes in one dimension (ie., [N]^dim)
 N = 1;
 CELL_OVERLAY_NX = 10;
-n = 103;
+n = 31;
 plotCurves = 1;
 
 %% 0: Regular Distribution; 1: Random Distribution; 2: Load Grid.
@@ -12,7 +12,7 @@ testNodeType=0
 
 
 global debug;
-debug =1; 
+debug =0; 
 
 
 if testNodeType==0
@@ -48,10 +48,53 @@ end
 
 addpath('int_morton')
 
-%% NEW:
-if debug
-    figure
-end
-[sten snodes ch cp] = knn_lsh(nodes, n, CELL_OVERLAY_NX, 3, @ijk_to_z );
+fprintf('Z-ordering\n');
+[sten snodes ch cp] = knn_lsh(nodes, n, CELL_OVERLAY_NX, 3, @ijk_to_z);
+figure(2)
+z_order.bandwidth = spy_stencils(sten);
+z_order.rcm_bandwidth = symrcm_stencils(sten);
+z_order.sten = sten;
+z_order.snodes = snodes; 
+z_order.ch = ch; 
+z_order.cp = cp; 
 
-spy_stencils(sten);
+fprintf('U-ordering\n');
+[sten snodes ch cp] = knn_lsh(nodes, n, CELL_OVERLAY_NX, 3, @ijk_to_u);
+figure(3)
+u_order.bandwidth = spy_stencils(sten);
+u_order.rcm_bandwidth = symrcm_stencils(sten);
+u_order.sten = sten;
+u_order.snodes = snodes; 
+u_order.ch = ch; 
+u_order.cp = cp; 
+
+fprintf('X-ordering\n');
+[sten snodes ch cp] = knn_lsh(nodes, n, CELL_OVERLAY_NX, 3, @ijk_to_x);
+figure(4)
+x_order.bandwidth = spy_stencils(sten);
+x_order.rcm_bandwidth = symrcm_stencils(sten);
+x_order.sten = sten;
+x_order.snodes = snodes; 
+x_order.ch = ch; 
+x_order.cp = cp; 
+
+fprintf('4 node Z-ordering\n');
+[sten snodes ch cp] = knn_lsh(nodes, n, CELL_OVERLAY_NX, 3, @ijk_to_4node_z);
+figure(5)
+node4_z_order.bandwidth = spy_stencils(sten);
+node4_z_order.rcm_bandwidth = symrcm_stencils(sten);
+node4_z_order.sten = sten;
+node4_z_order.snodes = snodes; 
+node4_z_order.ch = ch; 
+node4_z_order.cp = cp; 
+
+
+fprintf('IJK-ordering\n');
+[sten snodes ch cp] = knn_lsh(nodes, n, CELL_OVERLAY_NX, 3, @ijk_to_ijk);
+figure(6)
+ijk_order.bandwidth = spy_stencils(sten);
+ijk_order.rcm_bandwidth = symrcm_stencils(sten);
+ijk_order.sten = sten;
+ijk_order.snodes = snodes; 
+ijk_order.ch = ch; 
+ijk_order.cp = cp; 
--- matlab/spy_stencils.m
+++ matlab/spy_stencils.m
@@ -1,4 +1,4 @@
-function [] = spy_stencils(stencil_list)
+function [bandwidth] = spy_stencils(stencil_list)
 % Show the spy(stencil_list) to see sparsity patterns
 s = figure; 
 N = size(stencil_list, 1); 
@@ -8,6 +8,15 @@ for i = 1:N
     for j = 1:st
         A(i,stencil_list(i,j)) = 1; 
     end
+end
+
+if 1
+   r = symrcm(A)
+   
+   spy(A(r,r));
+   [ii,jj] = find(A(r,r)); 
+   bandwidth = max(abs(ii-jj))
+   pause
 end
 
 %% Find the bandwidth of the matrix
